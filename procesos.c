4da229c7d6792040a16aef3898d3b0f801bed0bb
Felix <felix.ramirez.27@unsch.edu.pe>
2026-09-21 17:21:01 -0500

    Cuarto commit - Prototipo de funcion para el registro

--- procesos.c
+++ procesos.c
@@ -9,7 +9,14 @@ typedef struct {
     int prioridad;
 } Proceso;
 
+// Prototipo de la función de registro
+void registrarProcesos(Proceso procesos[], int cantidad);
+
 int main(void) {
     Proceso listaProcesos[MAX_PROCESOS];
     return 0;
+}
+
+void registrarProcesos(Proceso procesos[], int cantidad) {
+    // Pendiente
 }
