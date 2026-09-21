5efe1b6f0ad0aa1942b556f0490c30be34a8981d
Felix <felix.ramirez.27@unsch.edu.pe>
2026-09-21 17:12:42 -0500

    Segundo commit - Definicion de la Estructura procesos

--- procesos.c
+++ procesos.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+// Parte A: Estructura Proceso
+typedef struct {
+    int pid;
+    char nombre[50];
+    int prioridad;
+} Proceso;
+
 int main(void) {
     printf("Sistema de Gestion de Procesos\n");
     return 0;
